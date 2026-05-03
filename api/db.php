afd5409fde6d6fefb8b3a4accde207f66cf6fe5a
JulianRodriguez24 <Julianlocus@gmail.com>
2026-05-03 16:19:03 -0500

    corrección db

--- api/db.php
+++ api/db.php
@@ -3,33 +3,29 @@ class Database {
 
     public static function connection() {
 
-        $hostname = "server-admin-pwa.mysql.database.azure.com";
-        $port     = "3306";
-        $database = "salas";
-
-        
-        $username = "adminphp";
-        $password = "Julian20667*";
-
-       
-        $ssl_ca = __DIR__ . "certs/DigiCertGlobalRootG2.crt.pem";
+        $host = "server-admin-pwa.mysql.database.azure.com";
+        $db   = "salas";
+        $user = "adminphp";
+        $pass = "Julian20667*";
 
         try {
             $pdo = new PDO(
-                "mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",
-                $username,
-                $password,
-                [
-                    PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ]
+                "mysql:host=$host;dbname=$db;charset=utf8",
+                $user,
+                $pass
             );
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             return $pdo;
 
         } catch (PDOException $e) {
-            throw new Exception("Error Azure: " . $e->getMessage());
+            echo json_encode([
+                "success" => false,
+                "error" => $e->getMessage()
+            ]);
+            exit;
         }
     }
 }
+?>
